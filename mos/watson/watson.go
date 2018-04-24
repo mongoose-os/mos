c1c8d9d826f08fe49bd3b6d925ff498c23bcf6a7
Deomid Ryabkov <rojer@cesanta.com>
2018-04-24 16:40:05 +0100

    Support custom orgs in `mos watson-iot-setup`
    CL: Support custom orgs in `mos watson-iot-setup`
    
    PUBLISHED_FROM=e3b487e1500d55efdb71bb32635db1afebea2789

--- mos/watson/watson.go
+++ mos/watson/watson.go
@@ -1,14 +1,22 @@
 package watson
 
 import (
+	"bytes"
 	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"strings"
 
 	"cesanta.com/common/go/ourutil"
 	"cesanta.com/mos/config"
 	"cesanta.com/mos/dev"
 	"github.com/cesanta/errors"
+	"github.com/golang/glog"
 	flag "github.com/spf13/pflag"
 )
 
@@ -18,41 +26,128 @@ const (
 )
 
 var (
-	watsonAPIKey      = ""
-	watsonAuthToken   = ""
-	watsonOrgID       = ""
-	watsonDeviceType  = ""
-	watsonDeviceID    = ""
-	watsonIoTHostName = ""
+	watsonAPIKeyFlag            = ""
+	watsonAPIAuthTokenFlag      = ""
+	watsonOrgIDFlag             = ""
+	watsonDeviceTypeFlag        = ""
+	watsonDeviceIDFlag          = ""
+	watsonMessagingHostNameFlag = ""
+	watsonAPIHostNameFlag       = ""
+	watsonDeviceAuthTokenFlag   = ""
 )
 
 func init() {
-	flag.StringVar(&watsonAPIKey, "watson-api-key", "", "IBM cloud API key")
-	flag.StringVar(&watsonAuthToken, "watson-auth-token", "", "IBM cloud API auth token")
-	flag.StringVar(&watsonOrgID, "watson-org-id", quickStartOrgID, "IBM cloud organization ID")
-	flag.StringVar(&watsonDeviceType, "watson-device-type", mosDeviceType, "IBM cloud device type")
-	flag.StringVar(&watsonDeviceID, "watson-device-id", "", "IBM cloud device ID")
-	flag.StringVar(&watsonIoTHostName, "watson-iot-host-name", "", "IBM cloud host name")
+	flag.StringVar(&watsonAPIKeyFlag, "watson-api-key", "", "IBM cloud API key")
+	flag.StringVar(&watsonAPIAuthTokenFlag, "watson-api-auth-token", "", "IBM cloud API auth token")
+	flag.StringVar(&watsonOrgIDFlag, "watson-org-id", quickStartOrgID, "IBM cloud organization ID")
+	flag.StringVar(&watsonDeviceTypeFlag, "watson-device-type", mosDeviceType, "IBM cloud device type")
+	flag.StringVar(&watsonDeviceIDFlag, "watson-device-id", "", "IBM cloud device ID")
+	flag.StringVar(&watsonAPIHostNameFlag, "watson-api-host-name", "", "IBM cloud API host name")
+	flag.StringVar(&watsonMessagingHostNameFlag, "watson-messaging-host-name", "", "IBM cloud host name")
+	flag.StringVar(&watsonDeviceAuthTokenFlag, "watson-device-auth-token", "", "IBM cloud device auth token")
 }
 
 func getOrgID() string {
-	if watsonOrgID != quickStartOrgID {
-		return watsonOrgID
+	if watsonOrgIDFlag != quickStartOrgID {
+		return watsonOrgIDFlag
 	}
-	if watsonAPIKey != "" {
-		parts := strings.Split(watsonAPIKey, "-")
+	if watsonAPIKeyFlag != "" {
+		parts := strings.Split(watsonAPIKeyFlag, "-")
 		if len(parts) == 3 {
 			return parts[1]
 		}
 	}
-	return watsonOrgID
+	return watsonOrgIDFlag
+}
+
+type watsonDeviceAdditionRequest struct {
+	DeviceID   string           `json:"deviceId,omitempty"`
+	AuthToken  string           `json:"authToken,omitempty"`
+	DeviceInfo watsonDeviceInfo `json:"deviceInfo,omitempty"`
+}
+
+type watsonDeviceInfo struct {
+	Description string `json:"description,omitempty"`
+	DeviceClass string `json:"deviceClass,omitempty"`
+}
+
+type watsonDeviceTypeInfo struct {
+	ID          string `json:"id,omitempty"`
+	Description string `json:"description,omitempty"`
+	ClassID     string `json:"classId,omitempty"`
+}
+
+type watsonDeviceTypeInfoList struct {
+	Results []watsonDeviceTypeInfo `json:"results"`
+}
+
+func watsonAPICall(reqType, hostName, apiKey, apiToken, api string, jsonReq, jsonResp interface{}) (int, error) {
+	client := &http.Client{}
+	url := fmt.Sprintf("https://%s/api/v0002%s", hostName, api)
+	var reqBody io.Reader
+	if jsonReq != nil {
+		jb, err := json.Marshal(jsonReq)
+		if err != nil {
+			return 0, errors.Annotatef(err, "invalid request body")
+		}
+		reqBody = bytes.NewBuffer(jb)
+	}
+	req, err := http.NewRequest(reqType, url, reqBody)
+	req.SetBasicAuth(apiKey, apiToken)
+	if reqType == "POST" {
+		req.Header.Add("Content-Type", "application/json")
+	}
+	glog.V(2).Infof("%s %s %s", reqType, url, reqBody)
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, errors.Annotatef(err, "API request failed (%s)", url)
+	}
+	b, _ := ioutil.ReadAll(resp.Body)
+	glog.V(2).Infof("Resp: %d %s", resp.StatusCode, string(b))
+	if resp.StatusCode >= 300 {
+		return resp.StatusCode, errors.Errorf("API request failed (%s): %d %s", url, resp.StatusCode, string(b))
+	}
+	defer resp.Body.Close()
+	if jsonResp != nil {
+		if err := json.Unmarshal(b, jsonResp); err != nil {
+			return 0, errors.Annotatef(err, "invalid response format %s", b)
+		}
+	}
+	return resp.StatusCode, nil
+}
+
+func checkDeviceType(hostName, devType, apiKey, apiToken string) error {
+	ourutil.Reportf("Checking device type %q...", devType)
+	var dtl watsonDeviceTypeInfoList
+	if _, err := watsonAPICall("GET", hostName, apiKey, apiToken, "/device/types", nil, &dtl); err != nil {
+		return errors.Annotatef(err, "failed to get device type list")
+	}
+	found := false
+	for _, dt := range dtl.Results {
+		if dt.ID == devType {
+			found = true
+			break
+		}
+	}
+	if !found {
+		ourutil.Reportf("  Creating device type %q...", devType)
+		dt := watsonDeviceTypeInfo{
+			ID:          devType,
+			Description: "Mongoose OS device type (created by mos watson-iot-setup)",
+			ClassID:     "Device",
+		}
+		if _, err := watsonAPICall("POST", hostName, apiKey, apiToken, "/device/types", &dt, &dtl); err != nil {
+			return errors.Annotatef(err, "failed to create device type %q", devType)
+		}
+	}
+	return nil
 }
 
 func WatsonIoTSetup(ctx context.Context, devConn *dev.DevConn) error {
 	var err error
 	orgID := getOrgID()
-	if orgID != quickStartOrgID && watsonAuthToken == "" {
-		return errors.Errorf("Org ID is provided but auth key is not set")
+	if orgID != quickStartOrgID && (watsonAPIKeyFlag == "" || watsonAPIAuthTokenFlag == "") {
+		return errors.Errorf("Org ID is provided but API key and auth token are not set")
 	}
 
 	ourutil.Reportf("Connecting to the device...")
@@ -66,7 +161,7 @@ func WatsonIoTSetup(ctx context.Context, devConn *dev.DevConn) error {
 	if err != nil {
 		return errors.Annotatef(err, "failed to get config")
 	}
-	devID := watsonDeviceID
+	devID := watsonDeviceIDFlag
 	if devID == "" {
 		devID, err = devConf.Get("device.id")
 		if err != nil {
@@ -81,21 +176,57 @@ func WatsonIoTSetup(ctx context.Context, devConn *dev.DevConn) error {
 
 	ourutil.Reportf("Org ID: %s", orgID)
 	ourutil.Reportf("Device ID: %s", devID)
-	authToken := ""
-	if watsonOrgID != quickStartOrgID {
-		// TODO(rojer): create device and device type if necessary.
+
+	apiHostName := watsonAPIHostNameFlag
+	if apiHostName == "" {
+		apiHostName = fmt.Sprintf("%s.internetofthings.ibmcloud.com", orgID)
+	}
+	messagingHostName := watsonMessagingHostNameFlag
+	if messagingHostName == "" {
+		messagingHostName = fmt.Sprintf("%s.messaging.internetofthings.ibmcloud.com", orgID)
 	}
 
-	hostName := watsonIoTHostName
-	if hostName == "" {
-		hostName = fmt.Sprintf("%s.messaging.internetofthings.ibmcloud.com", orgID)
+	devType := watsonDeviceTypeFlag
+	authToken := watsonDeviceAuthTokenFlag
+	if orgID != quickStartOrgID {
+		if err := checkDeviceType(apiHostName, devType, watsonAPIKeyFlag, watsonAPIAuthTokenFlag); err != nil {
+			return errors.Trace(err)
+		}
+		kb := make([]byte, 16)
+		rand.Read(kb)
+		authToken = base64.URLEncoding.EncodeToString(kb)[:20]
+		ourutil.Reportf("Creating device %q...", devID)
+		dcr := watsonDeviceAdditionRequest{
+			DeviceID:  devID,
+			AuthToken: authToken,
+			DeviceInfo: watsonDeviceInfo{
+				Description: "Created by mos watson-iot-setup",
+				DeviceClass: "Device",
+			},
+		}
+		code, err := watsonAPICall("POST", apiHostName, watsonAPIKeyFlag, watsonAPIAuthTokenFlag,
+			fmt.Sprintf("/device/types/%s/devices", devType), &dcr, nil)
+		if err != nil && code != 409 {
+			return errors.Annotatef(err, "failed to create device %q", devID)
+		} else if code == 409 {
+			ourutil.Reportf("  Already exists, deleting...", devID)
+			if _, err := watsonAPICall("DELETE", apiHostName, watsonAPIKeyFlag, watsonAPIAuthTokenFlag,
+				fmt.Sprintf("/device/types/%s/devices/%s", devType, devID), nil, nil); err != nil {
+				return errors.Annotatef(err, "failed to delete device %q", devID)
+			}
+			ourutil.Reportf("  Re-creating...")
+			if _, err := watsonAPICall("POST", apiHostName, watsonAPIKeyFlag, watsonAPIAuthTokenFlag,
+				fmt.Sprintf("/device/types/%s/devices", devType), &dcr, nil); err != nil {
+				return errors.Annotatef(err, "failed to re-create the device %q", devID)
+			}
+		}
 	}
 
 	newConf := map[string]string{
 		"device.id":        devID,
 		"watson.enable":    "true",
-		"watson.host_name": hostName,
-		"watson.client_id": fmt.Sprintf("d:%s:%s:%s", orgID, watsonDeviceType, devID),
+		"watson.host_name": messagingHostName,
+		"watson.client_id": fmt.Sprintf("d:%s:%s:%s", orgID, watsonDeviceTypeFlag, devID),
 	}
 	if authToken != "" {
 		newConf["watson.token"] = authToken
