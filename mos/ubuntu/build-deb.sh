248cc10c9e66506fd605acfcd0b76aa823b0d670
Deomid "rojer" Ryabkov <rojer@rojer.me>
2019-05-02 15:31:01 +0100

    Fix Ubuntu package build

--- mos/ubuntu/build-deb.sh
+++ mos/ubuntu/build-deb.sh
@@ -3,43 +3,40 @@
 PACKAGE=$1
 DISTR=$2
 VERSION=$3
+SRC=$PWD
 
 [ -z "${PACKAGE}" -o -z "${DISTR}" ] && { echo "Usage: $0 <package> <distr> [<version>]"; exit 1; }
 
-RECIPE_ORIG_FILE=$PWD/mos/ubuntu/${PACKAGE}-${DISTR}.recipe
+RECIPE="$SRC/mos/ubuntu/${PACKAGE}-${DISTR}.recipe"
 
-if [ -z "${VERSION}" ]; then
-  # Version was not given; use the recipe as it is
-  RECIPE=/src/mos/ubuntu/${PACKAGE}-${DISTR}.recipe
-  RECIPE_MOUNT=
-else
+if [ -n "${VERSION}" ]; then
   # Version is given: replace 1.11_CHANGE_ME with the actual version
-  RECIPE_TMP_DIR=$HOME/tmp/recipe-${PACKAGE}-${DISTR}-${VERSION}
+  RECIPE_TMP_DIR="$HOME/tmp/recipe-${PACKAGE}-${DISTR}-${VERSION}"
   mkdir -p ${RECIPE_TMP_DIR}
-  RECIPE_HOST_FILE=${RECIPE_TMP_DIR}/recipe
-
-  cat ${RECIPE_ORIG_FILE} | sed "s/1\.11_CHANGE_ME/${VERSION}/" > ${RECIPE_HOST_FILE}
-
-  RECIPE=/recipe/recipe
-  RECIPE_MOUNT=" -v ${RECIPE_TMP_DIR}:/recipe"
+  cat ${RECIPE} | sed "s/1\.11_CHANGE_ME/${VERSION}/" > ${RECIPE_TMP_DIR}/recipe
+  RECIPE="${RECIPE_TMP_DIR}/recipe"
 fi
 
 set -x -e
 
 IMAGE=docker.io/mgos/ubuntu-golang:${DISTR}
 
+if ! [ -d "$SRC/vendor/github.com" ]; then
+  make deps
+fi
+
 mkdir -p $HOME/tmp
 
+rm -rf $HOME/tmp/out-${DISTR}/*
 docker pull ${IMAGE}
 docker run -i -t --rm \
-    -v $PWD:/src \
+    -v $SRC:/src \
+    -v ${RECIPE}:/recipe \
     -v $HOME/tmp/out-${DISTR}:/tmp/work \
-    ${RECIPE_MOUNT} \
     ${IMAGE} \
     /bin/bash -l -c "\
-        cd /src && rm -rf /tmp/work/* && \
+        cd /src && \
         git-build-recipe --allow-fallback-to-native --package ${PACKAGE} --distribution ${DISTR} \
-            ${RECIPE} /tmp/work && \
+            /recipe /tmp/work && \
         cd /tmp/work/${PACKAGE} && \
-        rsync -a /src/go/ go/ && \
-        debuild --no-tgz-check -us -uc -S"
+        debuild --no-tgz-check -us -uc -b"
