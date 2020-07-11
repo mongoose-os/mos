408fbddeaa1b49e5817af13b937a1de97ade3a0e
Deomid "rojer" Ryabkov <rojer@rojer.me>
2020-07-11 19:40:58 +0100

    Add focal to the list of Ubuntu versions

--- version/version_util.go
+++ version/version_util.go
@@ -44,7 +44,7 @@ var (
 	regexpVersionNumber = regexp.MustCompile(`^\d+\.[0-9.]*$`)
 	regexpBuildIdDistr  = regexp.MustCompile(`^(?P<version>[^+]+)\+(?P<hash>[^~]+)\~(?P<distr>[^\d]+)\d+$`)
 
-	ubuntuDistrNames = []string{"xenial", "bionic", "disco", "eoan"}
+	ubuntuDistrNames = []string{"xenial", "bionic", "disco", "eoan", "focal"}
 )
 
 // GetMosVersion returns this binary's version, or "latest" if it's not a release build.
