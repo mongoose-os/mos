04356a0c7449ad151682219a9bd4e08f2f48ab84
QRPp <awesome.walrus+github@gmail.com>
2021-01-24 05:14:47 +0000

    Fix ignored library name overrides
    Clauses like `name: xyz' in .../path/to/not-xyz/mos.yml library manifest files
    didn't work, as in, the corresponding mgos_xyz_init(), HAVE_MGOS_XYZ etc names
    didn't correctly follow the name thus set.  Fix that by checking for library
    duplicates via location rather than name prior to reading the library manifest,
    then via the certainly correct name too.
    
    Ensure that the construct
    
      libs:
        - origin: .../path/to/not-xyz
          name: xyz
    
    (apparently useful for library overrides) never clashes with an in-library
    explicit name setting.
    
    Make the circumstances for `duplicate library' error messages impossible (keep
    the interdependent read and modification of libsHandled in parsing context
    within the same, uninterrupted streak of holding the parsing context mutex) and
    drop these messages.
    
    Add clarifying build log lines to help reviewing the intricacies of library
    reading, handling and name resolution.

---
+++ cli/manifest_parser/libs_by_loc_map.go
@@ -0,0 +1,62 @@
+//
+// Copyright (c) 2014-2019 Cesanta Software Limited
+// All rights reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package manifest_parser
+
+import (
+	"sync"
+	"github.com/mongoose-os/mos/cli/build"
+)
+
+type libByLoc struct {
+	Lib *build.SWModule
+	mtx sync.Mutex
+}
+
+type libByLocMap struct {
+	m   map[string]*libByLoc
+	mtx sync.Mutex
+}
+
+func newLibByLocStr() *libByLoc {
+	return &libByLoc{
+		mtx: sync.Mutex{},
+	}
+}
+
+func newLibByLocMap() *libByLocMap {
+	return &libByLocMap{
+		m:   map[string]*libByLoc{},
+		mtx: sync.Mutex{},
+	}
+}
+
+// AddOrFetchAndLock() tries to add a new location key to the set.  If
+// successful, the new entry (Lib: nil) is locked and returned; otherwise (the
+// location key already exists) the pre-existing entry is locked and returned.
+func (lm *libByLocMap) AddOrFetchAndLock(loc string) *libByLoc {
+	lm.mtx.Lock()
+	defer lm.mtx.Unlock()
+
+	ls, ok := lm.m[loc]
+	if !ok {
+	  ls = newLibByLocStr()
+	  lm.m[loc] = ls
+	}
+
+	ls.mtx.Lock()
+	return ls
+}
